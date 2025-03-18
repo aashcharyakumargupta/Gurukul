460d9a22400fb375132f44497cab63957452fea6
wicked-aashcharya <aashcharyakumargupta@gmail.com>
2025-03-18 23:34:42 +0530

    final

--- server/index.js
+++ server/index.js
@@ -21,12 +21,11 @@ app.use(bodyParser.urlencoded({extended: true }))
 app.use(express.json())
 
 const corsOption = {
-    origin: ['https://localhost:3000/','https://gurukul-sigma.vercel.app/'],
+    origin: ['https://localhost:3000','https://gurukul-sigma.vercel.app'],
     method: ["GET POST PUT DELETE HEAD PATCH"],
     credentials: true,
   };
-
-app.use(cors(corsOption))
+  app.use(cors(corsOption));
 
 mongoose
     .connect(process.env.MONGO_URL, {})
